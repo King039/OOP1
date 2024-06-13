8eb317ae919ec4ecb7dedf00230e73c4451677e5
bazanks039@gmail.com <bazanks039@gmail.com>
2024-06-13 20:51:39 +0300

    4

---
+++ 4/Company.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4
+{
+    internal class Company
+    {
+        private Person[]? personal;
+        public Person[]? Personal
+        {
+            get => personal; 
+            set => personal = value;
+        }
+
+        public Person this[int index]
+        {
+            get => personal![index];
+            set => personal![index] = value;
+        }
+    }
+}
---
+++ 4/Program.cs
@@ -0,0 +1,16 @@
+﻿using _4;
+// индексация 
+Person p1 = new Person() { Name = "Иванов И.И." };
+Person p2 = new Person() { Name = "Петров И.В." };
+Person p3 = new Person() { Name = "Сидоров С.И." };
+
+Person[] persons = new Person[3];
+persons[0] = p1;
+persons[1] = p2;
+persons[2] = p3;
+
+Company company = new Company { Personal = persons };
+
+Console.WriteLine(company[0].Name);
+Console.WriteLine(company[1].Name);
+Console.WriteLine(company[2].Name);
