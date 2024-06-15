d49b5c738bb98c44122fedaf01face58069c140c
bazanks039@gmail.com <bazanks039@gmail.com>
2024-06-15 19:47:03 +0300

    5

--- 5/Program.cs
+++ 5/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using _5;
+//Подсчет растояния между двух точек в системе координат Point
+
+//Point p1 = new Point(7, 9);
+//Point p2 = new Point(12, 10);
+//Console.WriteLine(Point.count);
+//Console.WriteLine($"Растояние между точками {Point.LenghtPoints(p1, p2):F2}");
+
+//Подсчет возраста до пенсии Person
+
+//Person person = new Person(60);
+//person.CheckAge();
+//Person.RetirementAge = 70; //изменили пенсионный взраст(государство)
+//person.CheckAge();
